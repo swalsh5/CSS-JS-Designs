9615a2236d4fe114974028ac8f49d0ad9e022c63
swalsh5 <sam.walsh31@gmail.com>
2021-02-05 19:56:26 +0000

    Added Bouncing Animation

--- Glowing Hover Animation/script.js
+++ Glowing Hover Animation/script.js
@@ -1,11 +1,31 @@
 function transform()
 {
-    var change = document.getElementById("l")
-    change.innerHTML = "i";
+    var change = document.getElementById("l");
+    //change.innerHTML = "i";
+    change.innerHTML = null;
+    createLetterI(change)
 }
 
 function transformBack()
 {
-    var change = document.getElementById("l")
+    var change = document.getElementById("l");
     change.innerHTML = "L";
+}
+//function for movable i character
+function createLetterI(change){
+    change.appendChild(createTop());
+    change.appendChild(createBottom());
+}
+function createTop() {
+    const topPart = document.createElement('span');
+    topPart.setAttribute("class", "character bounce");
+    topPart.innerHTML = "."
+    topPart.style.color = "orange";
+    return topPart;
+}
+function createBottom(){
+    const bottomPart = document.createElement('span')
+    bottomPart.setAttribute("class", "character");
+    bottomPart.innerHTML = "ı";
+    return bottomPart;
 }
